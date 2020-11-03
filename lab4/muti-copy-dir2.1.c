d0979891c2c5dcb97a51627770da6c6962a96edd
ZhangZhiQiang <2510406196@qq.com>
2020-11-03 14:14:49 +0800

    add note for muli-copy-dir2.1

--- lab4/muti-copy-dir2.1.c
+++ lab4/muti-copy-dir2.1.c
@@ -32,7 +32,7 @@ struct copy_tdata {
     int result;
 };
 
-// background copy file
+// background copy file，
 void background_copy_file(struct copy_tdata *pdata);
 
 
@@ -62,11 +62,11 @@ int main(int argc, char **argv){
 
 
 
-
+// 连接基础路径和子文件/文件夹的路径
 void path_cat(char *buf, const char *base, const char *child);
 
 
-
+// 生成字符串在堆中的副本
 char *new_str(const char*s){
     int len = strlen(s)+1;
     char*buf = (char*)malloc(len);
@@ -74,22 +74,24 @@ char *new_str(const char*s){
     return buf;
 }
 
+// 用源路径字符串和目的路径字符串生成 用于记录拷贝文件相关数据的结构体
 struct copy_tdata *new_copy_tdata(const char*src, const char *dst){
     struct copy_tdata* ans = (struct copy_tdata*)malloc(sizeof(struct copy_tdata));
-    ans->src = new_str(src);
-    ans->dst = new_str(dst);
+    ans->src = new_str(src); //由于需要在另一个线程运行，传入的参数不能在栈中，必须在使用在堆中的副本
+    ans->dst = new_str(dst); 
     ans->thread_err = 0;
     ans->result = unknown;
     return ans;
 }
 
+// 释放结构体
 void free_copy_tdata(struct copy_tdata* ppair){
     free(ppair->src);
     free(ppair->dst);
     free(ppair);
 }
 
-
+// 用于传入给线程的函数
 void *__copy_file(void *pvoid){
 
     struct copy_tdata *ppair = (struct copy_tdata*)pvoid;
@@ -113,7 +115,7 @@ void *__copy_file(void *pvoid){
 }
 
 
-
+// 使用给定的pdata，传入参数，生成线程后台拷贝文件，将线程信息记录在pdata中
 void background_copy_file(struct copy_tdata *pdata){
     // create a thread to copy file, data store into *pdata
     int err;
@@ -126,14 +128,14 @@ void background_copy_file(struct copy_tdata *pdata){
     }
 }
 
-// a list store thread data, to be manage
+// 一个简易的vector
 struct thread_list {
     struct copy_tdata ** list;
     size_t size;
     size_t capacity;
 };
 
-
+// vector.push_back
 void add_thread_data(struct thread_list *list, struct copy_tdata *th) {
 
     if (!list->list || list->size >= list->capacity) {
@@ -146,6 +148,7 @@ void add_thread_data(struct thread_list *list, struct copy_tdata *th) {
     list->list [list->size++] = th;
 
 }
+// vector.clear
 void free_thread_data(struct thread_list *list)
 {
 
@@ -155,12 +158,15 @@ void free_thread_data(struct thread_list *list)
 
 
 
-
+// 遍历路径和子文件夹，生成线程拷贝文件，所有的线程存到list中
 void __muti_copy_dir(const char *src, const char *dst, struct thread_list *list);
 
+
+// 多线程拷贝文件夹和子文件夹文件
 void muti_copy_dir(const char *src, const char *dst)
 {
 
+    // 初始化线程列表
     struct thread_list list;
     list.list = NULL;
     list.size = 0;
@@ -171,23 +177,21 @@ void muti_copy_dir(const char *src, const char *dst)
     int success_count = 0;
     int failed_count = 0;
     for(int i = 0 ;i < list.size; ++i){
-
         struct copy_tdata *data = list.list[i];
         pthread_join(data->tid, NULL);
         if (!data->thread_err && data->result == success)
             ++ success_count;
         else
             ++ failed_count;
-        //free_copy_tdata(data); do it in free list
+        
     }
     printf("\nCopy %ld files in total, %d success, %d failed\n", list.size, success_count, failed_count);
-
-    free_thread_data(&list);
+    free_thread_data(&list); //清除所有线程的相关数据
 
 }
 
 
-
+// 遍历路径和子文件夹，生成线程拷贝文件，所有的线程存到list中
 void __muti_copy_dir(const char *src, const char *dst, struct thread_list *list)
 {
     DIR * dir;
@@ -211,7 +215,6 @@ void __muti_copy_dir(const char *src, const char *dst, struct thread_list *list)
         path_cat(buf2, dst,ptr->d_name);
 
         if (ptr->d_type == DT_DIR){
-            printf("in %s %d\n", __FUNCTION__, __LINE__);
             __muti_copy_dir(buf, buf2, list);
         }
         else if (ptr->d_type == DT_REG){
@@ -233,7 +236,7 @@ void __muti_copy_dir(const char *src, const char *dst, struct thread_list *list)
 
 
 
-
+// 连接基础路径和子文件/文件夹的路径
 void path_cat(char *buf, const char *base, const char *child){
     int len = strlen(base);
     memmove(buf, base, len);
@@ -262,8 +265,8 @@ void copy_dir(const char *src, const char *dst)
     {
         if (!strcmp("..", ptr->d_name) || !strcmp(".", ptr->d_name))
             continue;
-        path_cat(buf, src,ptr->d_name);
-        path_cat(buf2, dst,ptr->d_name);
+        path_cat(buf, src, ptr->d_name);
+        path_cat(buf2, dst, ptr->d_name);
 
         if (ptr->d_type == DT_DIR){
             copy_dir(buf, buf2);
@@ -283,6 +286,7 @@ void copy_dir(const char *src, const char *dst)
 }
 
 
+// 拷贝文件
 int copy_file(const char *src, const char *dst){
 
 	int infd, outfd;
