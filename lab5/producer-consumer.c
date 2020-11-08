2e4a6def7794af3b9fb67bb016fe6de8da217ff3
ZhangZhiQiang <2510406196@qq.com>
2020-11-08 19:59:57 +0800

    producer-consumer note need get pool_mutex

--- lab5/producer-consumer.c
+++ lab5/producer-consumer.c
@@ -147,7 +147,7 @@ void *produce(void*argv){
 
         // 获取状态为empty的缓冲区区域，并设置状态为producing, 不成功在condp上wait，pool_empty_count 减1
         // 直至此线程设置pool_states[buf_id]为filled，才可以将shared_pool[buf_id]交给某个生产者
-        int buf_id = get_buf(empty, producing, &condp);
+        int buf_id = get_buf(empty, producing, &condp); // 需要获取pool_mutex
 
 
         // 生产
@@ -204,7 +204,7 @@ void *consume(void*argv){
 
         // 获取状态为filled的缓冲区区域，并设置状态为consuming, 不成功在condc上wait，pool_filled_count 减1
         // 直至此线程设置pool_states[buf_id]为empty之前，才能将shared_pool[buf_id]交给某个生产者
-        int buf_id = get_buf(filled, consuming, &condc);
+        int buf_id = get_buf(filled, consuming, &condc); // 需要获取pool_mutex
 
         struct product *ppro = shared_pool[buf_id];
 
