75b45e7eba2f7270a133cbd55481bfb047062c5e
ZhangZhiQiang <2510406196@qq.com>
2020-11-12 01:24:29 +0800

    producer-consumer2.1.0

--- lab5/producer-consumer.c
+++ lab5/producer-consumer.c
@@ -26,7 +26,7 @@ struct product *shared_pool[POOL_SIZE]; // 缓冲区
 int pool_empty_count = POOL_SIZE;
 int pool_filled_count = 0;
 
-enum pool_state_enum {empty, filled, producing, consuming};
+enum pool_state_enum {empty, filled, producing, consuming}; 
 int pool_states [POOL_SIZE]; // 缓冲区状态， 代替缓冲区每个区域一个mutex
 // 只有在状态为empty时，允许某个生产者访问
 // 只有在状态为filled时，允许某个消费者者访问
@@ -101,6 +101,17 @@ int get_buf(pool_state_enum test, pool_state_enum set, pthread_cond_t *cond) {
     }
 }
 
+void print_time(){
+    time_t tt = time(NULL);
+    struct tm* t=localtime(&tt);
+    printf("time: %d-%02d-%02d %02d:%02d:%02d", t->tm_year + 1900,
+        t->tm_mon + 1,
+        t->tm_mday,
+        t->tm_hour,
+        t->tm_min,
+        t->tm_sec);
+}
+
 
 void print_product(struct product*ppro){
 
@@ -134,18 +145,27 @@ void *produce(void*argv){
     int producer_id = (pthread_t*)argv - producer_tids;
     while(1){
         // 测试是否生产足够，producing_count 加1
-        pthread_mutex_lock(&count_mutex);
+        pthread_mutex_lock(&count_mutex); // 获得计数锁 读取更新产品计数
         int is_full = producted_count + producing_count == max_product;
         if (is_full) {
             pthread_mutex_unlock(&count_mutex);
             break;
         }
         ++ producing_count;
-        pthread_mutex_unlock(&count_mutex);
+        pthread_mutex_unlock(&count_mutex); // 释放计数锁
 
-        // 获取状态为empty的缓冲区区域，并设置状态为producing, 不成功在condp上wait，pool_empty_count 减1
+        // 获取状态为empty的缓冲区区域，并设置状态为producing, 暂不成功在condp上wait，pool_empty_count 减1
         int buf_id = get_buf(empty, producing, &condp); // 需获取pool_mutex，成功时释放pool_mutex
         // 直至此线程设置pool_states[buf_id]为filled，才可以将shared_pool[buf_id]交给某个生产者
+
+
+         // 打印生产前缓冲区状态
+        pthread_mutex_lock(&pool_mutex); // 获取缓冲区锁
+        printf("before %d produce %d in %d ", producer_id, product_id, buf_id);
+        print_time();
+        printf("\n");
+        print_pool();
+        pthread_mutex_unlock(&pool_mutex); // 释放缓冲区锁
 
         // 生产
         my_sleep();
@@ -158,20 +178,20 @@ void *produce(void*argv){
         ppro->t = time(NULL);
 
 
-        pthread_mutex_lock(&count_mutex);
+        pthread_mutex_lock(&count_mutex); // 获得计数锁 读取更新产品计数
         -- producing_count;
         ++ producted_count;
         int product_id = producted_count;
         ppro->product_id = product_id;
-        pthread_mutex_unlock(&count_mutex);
+        pthread_mutex_unlock(&count_mutex); // 释放计数锁
 
-        // 打印生产前后缓冲区状态
-        pthread_mutex_lock(&pool_mutex);
-        printf("before %d produce %d in %d\n", producer_id, product_id, buf_id);
-        print_pool();
+        // 打印生产后缓冲区状态
+        pthread_mutex_lock(&pool_mutex); // 获取缓冲区锁
+        
         shared_pool[buf_id] = ppro;
-
-        printf("after %d produce %d in %d\n", producer_id, product_id, buf_id);
+        printf("after %d produce %d in %d ", producer_id, product_id, buf_id);
+        print_time();
+        printf("\n");
         print_pool();
         printf("\n");
         // pthread_mutex_unlock(&pool_mutex);
@@ -180,7 +200,7 @@ void *produce(void*argv){
         pool_states[buf_id] = filled; // 更新buf_id处的缓冲区区域状态为filled
         ++ pool_filled_count; // pool_filled_count 加1
         pthread_cond_signal(&condc); //唤醒可能存在的某个正在等待的消费者
-        pthread_mutex_unlock(&pool_mutex);
+        pthread_mutex_unlock(&pool_mutex); // 释放缓冲区锁
     }
 }
 
@@ -188,7 +208,7 @@ void *consume(void*argv){
     int consumer_id = (pthread_t*)argv - consumer_tids;
     while(1) {
         // 测试是否消费够，consuming_count 加1
-        pthread_mutex_lock(&count_mutex);
+        pthread_mutex_lock(&count_mutex); // 获得计数锁 读取更新产品计数
         int is_enough = consumed_count + consuming_count == max_product;
 
         if (is_enough) {
@@ -197,32 +217,39 @@ void *consume(void*argv){
         }
 
         ++ consuming_count;
-        pthread_mutex_unlock(&count_mutex);
+        pthread_mutex_unlock(&count_mutex); // 释放计数锁
 
-        // 获取状态为filled的缓冲区区域，并设置状态为consuming, 不成功在condc上wait，pool_filled_count 减1
+        // 获取状态为filled的缓冲区区域，并设置状态为consuming, 暂不成功在condc上wait，pool_filled_count 减1
         int buf_id = get_buf(filled, consuming, &condc); // 需获取pool_mutex，成功时释放锁
         // 直至此线程设置pool_states[buf_id]为empty之前，才能将shared_pool[buf_id]交给某个生产者
 
-        struct product *ppro = shared_pool[buf_id];
+        // 打印消费时，消费后的缓冲区
+        pthread_mutex_lock(&pool_mutex); // 获取缓冲区锁
+        int product_id = ppro->product_id;
+        printf("before %d consume %d in %d ", consumer_id, product_id, buf_id);
+        print_time();
+        printf("\n");
+        print_pool();
+        pthread_mutex_unlock(&pool_mutex); // 释放缓冲区锁
 
         // 消费
         my_sleep();
-
+        
+        struct product *ppro = shared_pool[buf_id];
         ppro->comsumer_id = consumer_id;
 
-        pthread_mutex_lock(&count_mutex);
+        pthread_mutex_lock(&count_mutex); // 获得计数锁 读取更新产品计数
         -- consuming_count;
         ++ consumed_count;
-        pthread_mutex_unlock(&count_mutex);
+        pthread_mutex_unlock(&count_mutex); // 释放计数锁
 
         // 打印消费时，消费后的缓冲区
-        pthread_mutex_lock(&pool_mutex);
-        int product_id = ppro->product_id;
-        printf("before %d consume %d in %d\n", consumer_id, product_id, buf_id);
-        print_pool();
+        pthread_mutex_lock(&pool_mutex); // 获取缓冲区锁
         free(ppro);
         shared_pool[buf_id] = NULL;
-        printf("after %d consume %d in %d\n", consumer_id, product_id, buf_id);
+        printf("after %d consume %d in %d ", consumer_id, product_id, buf_id);
+        print_time();
+        printf("\n");
         print_pool();
         printf("\n");
         // pthread_mutex_unlock(&pool_mutex);
@@ -231,7 +258,7 @@ void *consume(void*argv){
         pool_states[buf_id] = empty; // 更新缓冲区区域状态为empty
         ++ pool_empty_count; // pool_empty_count 加1
         pthread_cond_signal(&condp); // 唤醒可能存在的某个正在等待的生产者
-        pthread_mutex_unlock(&pool_mutex);
+        pthread_mutex_unlock(&pool_mutex); // 释放缓冲区锁
     }
 }
 
