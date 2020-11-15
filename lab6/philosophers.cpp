c2b299385a2a7c2552d1c9e5b490efc75d6084f0
ZhangZhiQiang <2510406196@qq.com>
2020-11-15 21:42:25 +0800

    philosopher add note

--- lab6/philosophers.cpp
+++ lab6/philosophers.cpp
@@ -18,80 +18,91 @@
 
 
 struct philosopher{
-    static pthread_mutex_t mutex;
-    const static int PHILOSOPHER_SIZE = 5;
 
-    static philosopher ps[PHILOSOPHER_SIZE];
-    enum state_enum { eating, thinking, hungry };
+    static pthread_mutex_t mutex; // 锁
+    const static int PHILOSOPHER_SIZE = 5; // 哲学家数量
 
-    static constexpr const char *state_str[]{ "eating", "thinking", "hungry" };
+    static philosopher ps[PHILOSOPHER_SIZE]; // 哲学家们
+    enum state_enum { eating, thinking, hungry }; // 哲学家可能的状态
 
-    pthread_cond_t cond;
+    pthread_cond_t cond; // 哲学家拥有的条件变量
 
-    
-    state_enum state = thinking;
-    int id;
-    time_t last_t;
-    
+    state_enum state = thinking; // 哲学家当前的状态
+    int id; // 哲学家id
+    time_t last_t; // 上次改变状态时的时间
+
+private: // 禁止外部实例化哲学家
+
     philosopher(){
         pthread_cond_init(&cond, NULL);
         id = this - ps;
         last_t = 0;
         
     }
+
     ~philosopher(){
         pthread_cond_destroy(&cond);
     }
+public:
+
     philosopher &left()const {
         return ps[(id + PHILOSOPHER_SIZE - 1) % PHILOSOPHER_SIZE];
     }
+
     philosopher &right()const {
         return ps[(id + 1) % PHILOSOPHER_SIZE];
     }
 
-    void begin_eat(){
-        pthread_mutex_lock(&mutex);
-        state = hungry;
-        print_all(*this);
-        while (left().state == eating || right().state == eating) {
-            pthread_cond_wait(&cond, &mutex);
+    void begin_eat() {
+        // 打算就餐，条件不满足则等待
+        pthread_mutex_lock(&mutex); // 获得锁
+        this->state = hungry; // 更新状态为hungry
+        print_all(*this); // 打印
+        while (left().state == eating || right().state == eating) { 
+            // 若左边或右边的哲学家正在就餐
+            pthread_cond_wait(&this->cond, &mutex); // 等待
         }
-        state = eating;
-        print_all(*this);
-        pthread_mutex_unlock(&mutex);
+        this->state = eating; // 更新状态为就餐
+        print_all(*this); // 打印
+        pthread_mutex_unlock(&mutex); // 释放锁
     }
+
     void begin_think(){
-        pthread_mutex_lock(&mutex);
-        pthread_cond_signal(&left().cond);
-        pthread_cond_signal(&right().cond);
-        state = thinking;
-        print_all(*this);
-        pthread_mutex_unlock(&mutex);
+        // 开始思考
+        pthread_mutex_lock(&mutex); // 获得锁
+        pthread_cond_signal(&left().cond); // 唤醒可能正在等待的左边的哲学家
+        pthread_cond_signal(&right().cond); // 唤醒可能正在等待的右边的哲学家
+        this->state = thinking; // 更新状态为思考
+        print_all(*this); // 打印
+        pthread_mutex_unlock(&mutex); // 释放锁
     }
 
     static void *run(void *pp){
         
         philosopher &p = *(philosopher*)pp;
-        while(true) {            
+        
+        for (int i = 0; i <  10; ++i) {            
 
-            p.begin_eat();
-            
+            p.begin_eat(); // 打算就餐
+            // 开始就餐
             sleep(rand() % 9 + 2);            
-            
-            p.begin_think();
-
+            // 就餐结束
+            p.begin_think(); 
+            // 开始思考
             sleep(rand() % 6 + 3);
+            // 思考结束
         }
         return nullptr;
     }
+
     static void print_all(philosopher &p){
-        
+        // 打印所有的哲学家状态，p是状态刚发生改变的哲学家
         for (int i = 0; i < PHILOSOPHER_SIZE; ++i) {
             if (p.id == i) {
-                PRINT_FONT_RED;
+                PRINT_FONT_RED; // 红色字体，给发生改变的哲学家的状态
             }
             else {
-                PRINT_FONT_WHI;
+                PRINT_FONT_WHI; // 白色字体
             }
             switch (ps[i].state)
             {
@@ -115,7 +126,7 @@ struct philosopher{
         p.last_t = time(nullptr);
         PRINT_FONT_WHI;
         if (last_t == 0)
-            printf(" %10s    ", "");
+            printf(" %10s    ", ""); // 是第一次计时，状态为初始状态，没有上一状态维持的时间
         else  {
             switch (p.state)
             {
@@ -132,6 +143,7 @@ struct philosopher{
                 break;
             }
         }
+        // 打印时的时间
         printf(" %d-%02d-%02d %02d:%02d:%02d\n", 
             t->tm_year + 1900,
             t->tm_mon + 1,
